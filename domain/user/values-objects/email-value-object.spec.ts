10c914e4f3a415d35d7c636181504b28ecd0a8ec
Alessandrodev <alessandroadm@live.com>
2021-03-07 12:27:33 -0300

    test: ensure failure if provide invalid email

--- domain/user/values-objects/email-value-object.spec.ts
+++ domain/user/values-objects/email-value-object.spec.ts
@@ -13,4 +13,11 @@ describe('email-value-object.ts', () => {
     expect(email.isSuccess).toBe(true);
     expect(email.getResult().value).toBe('valid_mail@domain.com');
   });
+
+  it('should return fail if provide an invalid email', () => {
+    const email = EmailValueObject.create('invalid_email');
+
+    expect(email.isFailure).toBe(true);
+    expect(email.error).toBe('Invalid Email');
+  });
 });
